718d3f28c2a5e490e652692c0e98d04716c90a40
makrosloro <lorente.marcos@gmail.com>
2021-07-20 09:47:22 +0200

    Set up login trough Google popup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,46 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <section class="section">
+      <div class="container has-text-centered">
+        <button v-if="!user" @click="doLogin" class="button">Login with google</button>
+        <template v-else>
+          <h1 class="title has-text-centered">Hi {{ user.displayName }}!</h1>
+          <button @click="doLogout" class="button">Logout</button>
+        </template>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
+import {fb, auth} from "@/firebase";
+
 export default {
-  name: "Home"
+  name: "Home",
+  data(){
+    return {
+      user: null
+    };
+  },
+  methods: {
+    async doLogin(){
+      try {
+        const provider = new fb.auth.GoogleAuthProvider();
+        const user = await auth.signInWithPopup(provider);
+        this.user = user.user;
+      } catch (e) {
+        console.error(e.message)
+      }
+
+    },
+    async doLogout(){
+      try {
+        await auth.signOut();
+        this.user = null;
+      } catch (e){
+        console.error(e.message)
+      }
+    }
+  }
 };
 </script>
